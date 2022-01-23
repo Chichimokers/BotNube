91331de2adb52a73668d43d69e3be2b7746ef0ee
Ernes <ernestico833@gmail.com>
2022-01-23 14:11:00 +0100

    Oficial

--- Filesize.py
+++ Filesize.py
@@ -13,19 +13,29 @@ def CheckSize(size):
              finalse = size / 10000
 
              final = str(int(finalse))
+
              final = final[0]+"."+final[1]+final[2] +" Mb"
+
              if(size > 10000000):
+
                  finalse = size / 10000
 
                  final = str(int(finalse))
+
                  final = final[0]+final[1]+"."+final[2] +" Mb"
+
                  if(size > 100000000):
+
                      finalse = size / 10000
 
                      final = str(int(finalse))
+
                      final = final[0]+final[1]+final[2]+"."+final[4] +" Mb"
+
                      if(size >1000000000):
+
                          final = size /1000000000
+                         
                          final = str(final) +" GB"
 
 
