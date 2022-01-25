8fdb2184d7617ab23ec53402fc3e60c8a59f8990
Ernes <ernestico833@gmail.com>
2022-01-25 16:32:36 +0100

    OFicial

--- nubapi.py
+++ nubapi.py
@@ -612,7 +612,7 @@ class NubApi():
 
               if(mensajeuno.text.split(" ")[-1] != str(str(porcent)+"%")):
  
-                   lista = [100]
+                   lista = [1,10,30,40,60,80,90,100]
 
                    for e in lista:
 
