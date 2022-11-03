d17e477a5c09636cfcd29f5b33230c03a14b8c8d
Chichimokers <87203631+Chichimokers@users.noreply.github.com>
2022-11-03 01:04:16 +0100

    cracksito

--- freeapiPED.py
+++ freeapiPED.py
@@ -5,7 +5,7 @@ class Freeapi():
     def __init__(self) -> None:
 
         self.URI = "http://www.acimed.sld.cu/"
-
+        self.host = "www.acimed.sld.cu"
         self.username = "titilm30"
 
         self.password = "TitiLM30*"
@@ -24,7 +24,7 @@ class Freeapi():
 
         self.Session.headers.update({"Content-Type":"application/x-www-form-urlencoded"})
 
-        respuesta = self.Session.post(url="http://revpediatria.sld.cu/index.php/ped/login/signIn",data=data)
+        respuesta = self.Session.post(url="http://"+self.host+"/index.php/ped/login/signIn",data=data)
 
         
 
@@ -42,7 +42,7 @@ class Freeapi():
         'co mmentsToEditor': ""
            }
 
-        respano =  self.Session.post("http://revpediatria.sld.cu/index.php/ped/author/saveSubmit/1",data=data)
+        respano =  self.Session.post("http://"+self.host+"/index.php/ped/author/saveSubmit/1",data=data)
         
         respa = self.Session.get(url=respano.url)
 
@@ -70,11 +70,11 @@ class Freeapi():
 
            cabeceras = {"Content-Type":f.content_type}
 
-           paso2 = self.Session.post(url="http://revpediatria.sld.cu/index.php/ped/author/saveSubmit/"+submitnumber,data=f,headers=cabeceras)
+           paso2 = self.Session.post(url="http://"+self.host+"/index.php/ped/au+thor/saveSubmit/"+submitnumber,data=f,headers=cabeceras)
                     
            datas ="articleId="+articleID+"&formLocale=es_ES&deletedAuthors=&moveAuthor=0&moveAuthorDir=&moveAuthorIndex=&authors%5B0%5D%5BauthorId%5D=6133&authors%5B0%5D%5Bseq%5D=1&primaryContact=0&authors%5B0%5D%5BfirstName%5D=Alberto&authors%5B0%5D%5BmiddleName%5D=&authors%5B0%5D%5BlastName%5D=Alfonso+Perez&authors%5B0%5D%5Bemail%5D=axinotelegram%40gmail.com&authors%5B0%5D%5Borcid%5D=&authors%5B0%5D%5Burl%5D=&authors%5B0%5D%5Baffiliation%5D%5Bes_ES%5D=&authors%5B0%5D%5Bcountry%5D=&authors%5B0%5D%5BcompetingInterests%5D%5Bes_ES%5D=&authors%5B0%5D%5Bbiography%5D%5Bes_ES%5D=&title%5Bes_ES%5D=test&abstract%5Bes_ES%5D=ddd&subject%5Bes_ES%5D=&language=es&sponsor%5Bes_ES%5D="
           
-           paso3 = self.Session.post(url="http://revpediatria.sld.cu/index.php/ped/author/saveSubmit/"+str(int(submitnumber)+1),data=datas)
+           paso3 = self.Session.post(url="http://"+self.host+"/index.php/ped/author/saveSubmit/"+str(int(submitnumber)+1),data=datas)
            
                   
            namessplit = filepath.split('/')
@@ -159,7 +159,7 @@ class Freeapi():
 
            finalnumber = subnumber.split("/")
 
-           finalizar = self.Session.post(url="http://revpediatria.sld.cu/index.php/ped/author/saveSubmit/"+finalnumber[len(finalnumber)-1],data=datosfinal)
+           finalizar = self.Session.post(url="http://"+self.host+"/index.php/ped/author/saveSubmit/"+finalnumber[len(finalnumber)-1],data=datosfinal)
 
            print(finalizar.url)
            
