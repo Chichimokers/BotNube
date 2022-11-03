d85acb51415b42a3931d54786dbbbaafb6e06e6b
Chichimokers <87203631+Chichimokers@users.noreply.github.com>
2022-11-03 01:37:01 +0100

    g

--- freeapiPED.py
+++ freeapiPED.py
@@ -70,7 +70,7 @@ class Freeapi():
 
            cabeceras = {"Content-Type":f.content_type}
 
-           paso2 = self.Session.post(url="http://"+self.host+"/index.php/acimed/au+thor/saveSubmit/"+submitnumber,data=f,headers=cabeceras)
+           paso2 = self.Session.post(url="http://"+self.host+"/index.php/acimed/author/saveSubmit/"+submitnumber,data=f,headers=cabeceras)
                     
            datas ="articleId="+articleID+"&formLocale=es_ES&deletedAuthors=&moveAuthor=0&moveAuthorDir=&moveAuthorIndex=&authors%5B0%5D%5BauthorId%5D=6133&authors%5B0%5D%5Bseq%5D=1&primaryContact=0&authors%5B0%5D%5BfirstName%5D=Alberto&authors%5B0%5D%5BmiddleName%5D=&authors%5B0%5D%5BlastName%5D=Alfonso+Perez&authors%5B0%5D%5Bemail%5D=axinotelegram%40gmail.com&authors%5B0%5D%5Borcid%5D=&authors%5B0%5D%5Burl%5D=&authors%5B0%5D%5Baffiliation%5D%5Bes_ES%5D=&authors%5B0%5D%5Bcountry%5D=&authors%5B0%5D%5BcompetingInterests%5D%5Bes_ES%5D=&authors%5B0%5D%5Bbiography%5D%5Bes_ES%5D=&title%5Bes_ES%5D=test&abstract%5Bes_ES%5D=ddd&subject%5Bes_ES%5D=&language=es&sponsor%5Bes_ES%5D="
           
